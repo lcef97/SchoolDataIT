691c858c460b06b4f8a27e6e7d1de4a96f8b76c0
Leonardo Cefalo <leonardo.cefalo@uniba.it>
2025-04-10 11:23:40 +0200

    proper version of Get_AdmUnNames, hope R checks are OK with API using

--- R/Get_AdmUnNames.R
+++ R/Get_AdmUnNames.R
@@ -4,8 +4,8 @@
 #'
 #' @param Year Numeric or character value. Last available is 2024.
 #' For coherence with school data, it is also in the formats: \code{2023}, \code{"2022/2023"}, \code{202223}, \code{20222023}. \code{2023} by default.
-#' @param date Character. The reference date, in format \code{"mm_dd"}, either \code{"01_01"} \code{"06_30"},
-#' or \code{"09_01"} (close to the beginning of the school year). \code{"01_01"} by default.
+#' @param date Character. The reference date, in format \code{"mm_dd"}, e.g. \code{"01_01"} for January 1st or,
+#'  \code{"09-01"} for the beginning of the school year. \code{"01_01"} by default.
 #' @param autoAbort Logical. Whether to automatically abort the operation and return NULL in case of missing internet connection or server response errors. \code{FALSE} by default.
 #'
 #' @return An object of class \code{tbl_df}, \code{tbl} and \code{data.frame}, including: NUTS-3 code, NUTS-3 abbreviation,
@@ -22,31 +22,60 @@
 
 
 
-Get_AdmUnNames <- function(Year = 2023, date = "01_01", autoAbort = FALSE){
+Get_AdmUnNames_auto <- function(Year = 2023, date = "01_01", autoAbort = FALSE){
 
 
   if(!Check_connection(autoAbort = autoAbort)) return(NULL)
 
-  pattern0 <- "https://raw.githubusercontent.com/lcef97/ISTAT_AdmUnNames/main/ISTAT_AdmUnNames"
-
-  while(!date %in% c("01_01", "06_30", "09_01")) {
-    message("Please, choose either '01_01', '06_30' or '09_01' as date")
-    date <- readline(prompt = "  > ")
-  }
+  pattern0 <- c("https://situas.istat.it/ShibO2Module/api/Report/Spool/", "/61?&pdoctype=CSV")
+  date <- stringr::str_replace(date, "_", "-")
+  #while(!date %in% c("01-01", "06-30", "09-01")) {
+  # message("Please, choose either '01-01', '06-30' or '09-01' as date")
+  #date <- readline(prompt = "  > ")
+  #}
   while(!as.numeric(Year) %in% c(2015:2024)){
     message("Please, choose a year between 2015 and 2024")
-    year <- readline(prompt = "  > ")
+    Year <- readline(prompt = "  > ")
   }
   Year <- 2000 + as.numeric(year.patternA(Year))%%100
-  pattern1<- paste0(Year, "_", date, ".CSV")
+  url<- paste0(pattern0[1], Year, "-", date, pattern0[2])
+  json_body <- "{}"
 
-  url <- paste0(pattern0, "/", Year, "/", pattern1)
+
+  status <- 0
+  attempt <- 0
+  while(status != 200 && attempt <= 10){
+
+    response <- tryCatch({httr::POST(url,
+                                     body = json_body,
+                                     encode = "raw",
+                                     httr::add_headers(
+                                       `Content-Type` = "application/json-patch+json",
+                                       `User-Agent` = "R (httr package)",
+                                       `Origin` = "https://situas.istat.it",
+                                       `Referer` = "https://situas.istat.it/web/"))
+    }, error = function(e) return(NULL))
+
+    status <- response$status_code
+    if(is.null(response)){
+      status <- 0
+    }
+    if(status != 200){
+      attempt <- attempt + 1
+      message("Operation exited with status: ", status, "; operation repeated (",
+              10 - attempt, " attempts left)")
+    }
+    if(attempt >= 10) {
+      message("Maximum attempts reached. Abort. We apologise for the inconvenience")
+      return(NULL)
+    }
+  }
 
   DB <- NULL
   attempt <- 0
   while(is.null(DB) && attempt <= 10){
     DB <- tryCatch({
-      readr::read_delim(url, delim = ";", show_col_types = FALSE)
+      readr::read_delim(rawToChar(httr::content(response, type = "raw")), delim = ";", show_col_types = FALSE)
     }, error = function(e){
       message("Cannot read the file; ", 10 - attempt,
               " attempts left. If the problem persists, please contact the maintainer.\n")
